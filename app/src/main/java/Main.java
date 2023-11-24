e468a2cb147c42b1bb11d2abb4dd32f7de6fe211
Masaki Gima <e235702@ie.u-ryukyu.ac.jp>
2023-11-24 15:07:21 +0900

    report4 step2-1

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,8 +2,8 @@ import jp.ac.uryukyu.ie.e235702.*;
 
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
+        Warrior hero = new Warrior("勇者", 10, 10);
+        Enemy enemy = new Enemy("スライム", 3, 2);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
@@ -11,7 +11,7 @@ public class Main {
         while( hero.isDead() == false && enemy.isDead() == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
-            hero.attack(enemy);
+            hero.attackWithWeponSkill(enemy);
             enemy.attack(hero);
         }
         System.out.println("戦闘終了");
